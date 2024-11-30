0a377382510e1c8b27b455b20e8659a3c7236f66
EleneBB <bibilashvili.cn@propertyguru.tech>
2024-11-30 01:40:31 +0100

    modify swagger setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,23 +3,41 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   dotenv.config();
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.use(cookieParser());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/docs/',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('D&D Connect API')
     .setDescription('API documentation for the D&D Connect application')
     .setVersion('1.0')
+    .addServer(
+      process.env.NODE_ENV === 'production'
+        ? 'https://dnd-connect-8375.vercel.app'
+        : 'http://localhost:3001',
+    )
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    customCssUrl: 'swagger-ui.css',
+  });
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://dnd-connect.vercel.app'
+        : 'http://localhost:3000',
     credentials: true,
   });
 
